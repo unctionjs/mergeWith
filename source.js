0763f82c1131b0160599c66ee64dbcc84586cea2
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-14 01:08:00 -0700

    Using internal packages and fixing types

--- source.js
+++ source.js
@@ -1,13 +1,11 @@
 import reduceWithValueKey from "@unction/reducewithvaluekey"
+import fetchKey from "@unction/key"
 import attach from "@unction/attach"
 
 export default function mergeWith (unction: ValueType => ValueType => ValueType): Function {
-  return reduceWithValueKey((accumulated: AccumulatedType): Function => (value: ValueType): Function => (key: KeyType): IterableType => {
-    if (accumulated[key]) {
-      return {
-        ...accumulated,
-        [key]: unction(accumulated[key])(value),
-      }
+  return reduceWithValueKey((accumulated: IterableType): Function => (value: ValueType): Function => (key: KeyType): IterableType => {
+    if (fetchKey(key)(accumulated)) {
+      return attach(key)(unction(fetchKey(key)(accumulated))(value))(accumulated)
     }
 
     return attach(key)(value)(accumulated)
