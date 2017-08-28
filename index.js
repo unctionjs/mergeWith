21863884c83e24e392d44aeffbea1d38ac046bc1
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-27 19:46:29 -0700

    We have a new, more formal, name for iterables

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import fetchKey from "@unction/key"
 import attach from "@unction/attach"
 
 export default function mergeWith (unction: ValueType => ValueType => ValueType): Function {
-  return reduceWithValueKey((accumulated: IterableType): Function => (value: ValueType): Function => (key: KeyType): IterableType => {
+  return reduceWithValueKey((accumulated: FunctorType): Function => (value: ValueType): Function => (key: KeyType): FunctorType => {
     if (fetchKey(key)(accumulated)) {
       return attach(key)(unction(fetchKey(key)(accumulated))(value))(accumulated)
     }
