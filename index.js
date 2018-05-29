9e1d47cabc974969edc427a183c85be27b267e2e
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-28 17:32:08 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-import reduceWithValueKey from "@unction/reducewithvaluekey"
-import fetchKey from "@unction/key"
-import attach from "@unction/attach"
-
-export default function mergeWith (unction: ValueType => ValueType => ValueType): Function {
-  return reduceWithValueKey((accumulated: FunctorType): Function => (value: ValueType): Function => (key: KeyType): FunctorType => {
+import reduceWithValueKey from "@unction/reducewithvaluekey";
+import fetchKey from "@unction/key";
+import attach from "@unction/attach";
+export default function mergeWith(unction) {
+  return reduceWithValueKey(accumulated => value => key => {
     if (fetchKey(key)(accumulated)) {
-      return attach(key)(unction(fetchKey(key)(accumulated))(value))(accumulated)
+      return attach(key)(unction(fetchKey(key)(accumulated))(value))(accumulated);
     }
 
-    return attach(key)(value)(accumulated)
-  })
+    return attach(key)(value)(accumulated);
+  });
 }
